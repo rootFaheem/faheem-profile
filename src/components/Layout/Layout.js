d167017cbd228320134bf20a843fd7e4bd6d6c73
faheem <faheem@faheems-MacBook-Air.local>
2019-01-08 17:34:12 +0530

    adding css-module

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Layout.css';
+
+const layout = (props) => (
+    <div className={classes.Layout}>
+        <h1 className={classes.Layout}>Hi! I am Faheem :)</h1>
+    </div>
+);
+
+export default layout;
