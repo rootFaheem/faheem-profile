cdb802b27a7b52a4517c9cbfd6af9ed4c85bea7e
faheem <faheem@faheems-MacBook-Air.local>
2019-01-11 21:23:14 +0530

    adding linear-gradient in Landing page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -6,7 +6,7 @@ const landing = (props) => (
         <h1>Hello, I am Faheem Ahmad <br />
         a Full Stack Web Developer </h1>
         <p>I love what i do, and vice-versa.</p>
-        <button>See My Work &rarr;</button>
+        <span><button >View My Work &rarr;</button></span>
     </div>
 );
 
--- src/components/Toolbar/Logo/Logo.js
+++ src/components/Toolbar/Logo/Logo.js
@@ -2,10 +2,11 @@ import React from 'react';
 import brandlogo from '../../../assets/logo1.png';
 import classes from './Logo.css';
 
-const logo = (props) => (
-    <div className={classes.Logo}> 
-        <img src={brandlogo} alt="MyLogo"></img>
-    </div>
-);
+const logo = (props) => {
 
+  return  ( <div className={classes.Logo} > 
+            <a className="tooltip" href={"/"}> <img src={brandlogo} alt="MyLogo"></img></a>
+        </div>
+  )};
+    
 export default logo;
